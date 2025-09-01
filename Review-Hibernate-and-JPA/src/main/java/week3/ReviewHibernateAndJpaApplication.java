688ebc3fe0c7becb2dcae98920e40545eaa22a23
Pedro Maximo <pedromaximocc@gmail.com>
2025-09-01 09:16:20 -0300

    voltando a pratica de spring iniciando o review para hibernate/jpa relacionamentos entre outros para poder avancar na materia

---
+++ Review-Hibernate-and-JPA/src/main/java/week3/ReviewHibernateAndJpaApplication.java
@@ -0,0 +1,13 @@
+package week3.Review.Hibernate.and.JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewHibernateAndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewHibernateAndJpaApplication.class, args);
+	}
+
+}
---
+++ Review-Hibernate-and-JPA/src/test/java/week3/Review/Hibernate/and/JPA/ReviewHibernateAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package week3.Review.Hibernate.and.JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewHibernateAndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
