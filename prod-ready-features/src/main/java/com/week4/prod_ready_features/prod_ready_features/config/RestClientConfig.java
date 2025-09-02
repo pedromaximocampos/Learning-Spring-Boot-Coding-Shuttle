9995200b1d3b986a2f4873c1500f8fc384e6d001
Pedro Maximo <pedromaximocc@gmail.com>
2025-09-02 14:37:24 -0300

    feat: trando erro geral das respostas de apis de terceiros

--- prod-ready-features/src/main/java/com/week4/prod_ready_features/prod_ready_features/config/RestClientConfig.java
+++ prod-ready-features/src/main/java/com/week4/prod_ready_features/prod_ready_features/config/RestClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
@@ -23,6 +24,9 @@ public class RestClientConfig {
         return RestClient.builder()
                 .baseUrl(BASE_URL_EMPLOYEE)
                 .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .defaultStatusHandler(HttpStatusCode::is5xxServerError,(req, res) ->{
+                            throw new RuntimeException("Employee Service is down. Please try again later.");
+                        })
                 .build();
     }
 
