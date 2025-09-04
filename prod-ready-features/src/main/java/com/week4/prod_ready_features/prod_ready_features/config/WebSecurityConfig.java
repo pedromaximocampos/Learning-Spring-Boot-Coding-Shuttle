2bf74a276c39d4f91d555710d88f94e4ecbffba1
Pedro Maximo <pedromaximocc@gmail.com>
2025-09-04 11:39:49 -0300

    feat: iniciando o entendimento de como configurar WebSecurityConfig do SpringSecurity

---
+++ prod-ready-features/src/main/java/com/week4/prod_ready_features/prod_ready_features/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.week4.prod_ready_features.prod_ready_features.config;
+
+import jakarta.servlet.annotation.WebServlet;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/post", "/error", "/public/**").permitAll()
+                        .requestMatchers("/posts/**").hasAnyRole( "ADMIN")
+                        .anyRequest().authenticated())
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig -> sessionConfig
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return httpSecurity.build();
+    }
+
+
+
+}
